8185f33eff6146f622e83d31737874ae79015f8f
Hamdy Mohamed <162215675+HamdyMohamed4@users.noreply.github.com>
2024-10-21 20:46:47 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,10 @@
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
+  
+SELECT * 
+FROM Customers 
+WHERE customer_id = 2;
+
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
