e6b9f9cef775946aa3d1af451b30b250b72c0aaa
Hamdy Mohamed <162215675+HamdyMohamed4@users.noreply.github.com>
2024-10-21 20:31:35 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,4 @@
--- Retrieve full description of the Books table using information_schema
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
-FROM 
-    information_schema.COLUMNS
-WHERE 
-    TABLE_NAME = 'Books' 
-    AND TABLE_SCHEMA = DATABASE();
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
