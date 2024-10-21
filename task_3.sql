01ba92ef868786a10c6949faa3b7772a45c293ed
Hamdy Mohamed <162215675+HamdyMohamed4@users.noreply.github.com>
2024-10-21 20:00:29 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -38,3 +38,5 @@ CREATE TABLE Order_Details (
 
 -- List all tables in the alx_book_store database
 SHOW TABLES FROM alx_book_store;
+
+mysql -u your_username -p your_password alx_book_store < task_3.sql
